13bf2bc3aa4d8aae79ea4f037c872392065929e4
sirdan <stanford303@gmail.com>
2022-08-23 16:30:04 -0700

    added userservices and sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE user (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    fisrt_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE,
+    passowrd_hash VARCHAR NOT NULL
+);
