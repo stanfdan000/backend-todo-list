205189f192d956a2c2bea302d0d8ad1b84f6ca69
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-24 14:39:03 -0700

    built out controllers and model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS todo_list CASCADE;
 
 
 CREATE TABLE users (
@@ -9,4 +10,13 @@ CREATE TABLE users (
     last_name VARCHAR NOT NULL,
     email VARCHAR UNIQUE,
     password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE todo_list (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    details TEXT NOT NULL,
+    status BOOLEAN DEFAULT FALSE,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
